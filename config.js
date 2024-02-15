86fabc25e3183b2dde0d4f8f37c2fc4023f5e6a4
Darkysimx <111804476+Darkysimx@users.noreply.github.com>
2024-02-15 15:11:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDxxDObept2fRjUeAbPwm3pFGZ5CiHHNnk",
+    authDomain: "pro71-741f7.firebaseapp.com",
+    projectId: "pro71-741f7",
+    storageBucket: "pro71-741f7.appspot.com",
+    messagingSenderId: "27420184242",
+    appId: "1:27420184242:web:bd10004bf203b6127143ab"
+  };
+  
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
